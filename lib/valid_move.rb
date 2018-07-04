c84330111282fb1c5b70878f206efd82b1014873
Louis Maldonado <louis.maldm@gmail.com>
2018-07-04 01:31:48 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,6 +1,7 @@
 require 'pry'
 
 def valid_move?(board, index)
+  binding.pry
   if board[index].between?(0, 8)
     position_taken?
   else
@@ -9,7 +10,6 @@ end
 end
 
 def position_taken?(board, index)
-  binding.pry
   if board[index] == " " || board[index] == ""
     true
   else
