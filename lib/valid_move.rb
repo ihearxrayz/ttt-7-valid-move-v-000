7daa468a4c08b2caa27678b0a06ccc5db53169b2
Louis Maldonado <louis.maldm@gmail.com>
2018-07-04 01:42:11 +0000

    automatically backed up by learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def valid_move?(board, index)
-  index.between?(0, 8) && position_taken?(board,index)
+  index.between?(0, 8) && !position_taken?(board,index)
 end
 
 def position_taken?(board, index)
